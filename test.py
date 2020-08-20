6ee30462d8fd5ecb1145cb7474a4a02454e89d73
ccordero90 <57953968+ccordero90@users.noreply.github.com>
2020-08-19 22:35:07 -0400

    Add files via upload

--- test.py
+++ test.py
@@ -6,12 +6,12 @@ import os
 import sys
 
 sys.path.insert(0, os.getcwd())
-filename_prefix = "test"
-csv_file = "test.csv"
-pdf_file = "test.pdf"
+filename_prefix = "formulario"
+csv_file = "formulario.csv"
+pdf_file = "formulario.pdf"
 tmp_file = "tmp.fdf"
 output_folder = './output/'
-os.environ['PATH'] += os.pathsep + 'C:\\Program Files (x86)\\PDFtk\\bin;'
+#os.environ['PATH'] += os.pathsep + 'C:\\Program Files (x86)\\PDFtk\\bin;'  #uncomment if in windows
 
 def process_csv(file):
     headers = []
@@ -32,7 +32,7 @@ def form_fill(fields):
   fdf_file = open(tmp_file,"wb")
   fdf_file.write(fdf)
   fdf_file.close()
-  output_file = '{0}{1} {2}.pdf'.format(output_folder, filename_prefix, fields[1][1])
+  output_file = '{0}{1}_{2}.pdf'.format(output_folder, filename_prefix, fields[1][1])
   cmd = 'pdftk "{0}" fill_form "{1}" output "{2}" dont_ask flatten'.format(pdf_file, tmp_file, output_file)
   os.system(cmd)
   os.remove(tmp_file)
@@ -43,5 +43,5 @@ print('-----------------------')
 for i in data:
   if i[0][1] == 'Yes':
     continue
-  print('{0} {1} created...'.format(filename_prefix, i[1][1]))
+  print('{0}_{1} created...'.format(filename_prefix, i[1][1]))
   form_fill(i)
